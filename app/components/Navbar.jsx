db80a8fa01febe682986e1ebc91ebec4e485bb53
Mehrnz <mehrnzv@gmail.com>
2024-08-08 12:57:23 +0330

    fixed bugs

--- app/components/Navbar.jsx
+++ app/components/Navbar.jsx
@@ -1,7 +1,7 @@
 "use client";
 import Link from "next/link";
 import { navlinks } from "../constants/data";
-import NavLink from "./NavLink";
+import NavLink from "./Navlink";
 import { FiMenu } from "react-icons/fi";
 import { AiOutlineClose } from "react-icons/ai";
 import { useState } from "react";
